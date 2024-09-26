fb2954149a5f1c051689ff57636c2154a9ec878a
Kubbaist <123012930+Kubbaist@users.noreply.github.com>
2024-09-26 14:31:38 +0200

    created documentation for the data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Wiki Graph
+#' 
+#' A dataset representing a graph with its nodes and weighted edges. Every row
+#' represents one edge between two nodes.
+#'
+#' @format ## `wiki_graph`
+#' A data frame with 18 rows and 3 columns:
+#' \describe{
+#'   \item{v1}{Vertex 1}
+#'   \item{v2}{Vertex 2}
+#'   \item{w}{Weight of the edge}
+#' }
+"wiki_graph"
